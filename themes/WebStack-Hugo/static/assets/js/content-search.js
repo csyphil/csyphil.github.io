abc6abb4e7ceab99d3c2c3339914a9dfc36ec012
philsilver <philsilver31925@gmail.com>
2025-03-20 05:08:41 +0800

    Update content-search.js

--- themes/WebStack-Hugo/static/assets/js/content-search.js
+++ themes/WebStack-Hugo/static/assets/js/content-search.js
@@ -1,53 +1,18 @@
 $(function() {
-    var $word = $('#word');
     var $searchText = $('#search-text');
 
-    $searchText.keyup(function() {
-        var keywords = $(this).val();
-        if (keywords == '') {
-            $word.hide();
-            return;
-        }
-
-        $.ajax({
-            url: 'https://suggestion.baidu.com/su?wd=' + keywords,
-            dataType: 'jsonp',
-            jsonp: 'cb',
-            beforeSend: function() {},
-            success: function(res) {
-                $word.empty().show();
-                var hotList = res.s.length;
-                if (hotList) {
-                    $word.css("display", "block");
-                    for (var i = 0; i < hotList-1; i++) {
-                        var li = $("<li><span>" + (i + 1) + "</span>" + res.s[i] + "</li>");
-                        if (i === 0) {
-                            li.css({ "border-top": "none" });
-                        } else if (i === 1) {
-                            li.find("span").css({ "color": "#fff", "background": "#ff8547" });
-                        } else if (i === 2) {
-                            li.find("span").css({ "color": "#fff", "background": "#ffac38" });
-                        }
-                        $word.append(li);
-                    }
-                } else {
-                    $word.hide();
-                }
-            },
-            error: function() {
-                $word.empty().show();
-                $word.hide();
+    // 监听回车键来触发搜索
+    $searchText.keyup(function(event) {
+        if (event.keyCode === 13) {  // 如果按下回车键（Enter）
+            var word = $(this).val();
+            if (word !== '') {
+                // 这里可以执行你的搜索操作，例如提交表单或者发起搜索请求
+                $('.submit').trigger('click');  // 假设这里是触发搜索提交按钮的点击事件
             }
-        });
+        }
     });
 
-    $word.on('click', 'li', function() {
-        var word = $(this).text().replace(/^[0-9]/, '');
-        $searchText.val(word);
-        $word.empty().hide();
-        $('.submit').trigger('click');
-    });
-
+    // 如果点击了搜索框外面，关闭搜索词建议（如果需要）
     $(document).on('click', '.io-grey-mode', function() {
         $word.empty().hide();
     });
