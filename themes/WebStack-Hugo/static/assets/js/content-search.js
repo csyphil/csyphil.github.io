d551a3a3727e222c40940e757a72b1285e356c93
philsilver <philsilver31925@gmail.com>
2025-03-20 04:44:05 +0800

    update

--- themes/WebStack-Hugo/static/assets/js/content-search.js
+++ themes/WebStack-Hugo/static/assets/js/content-search.js
@@ -1,54 +1,44 @@
 $(function() {
     var $word = $('#word');
     var $searchText = $('#search-text');
-    var debounceTimeout;
-    var currentIndex = -1;
 
-    $searchText.keyup(function(e) {
-        clearTimeout(debounceTimeout);
+    $searchText.keyup(function() {
         var keywords = $(this).val();
         if (keywords == '') {
             $word.hide();
             return;
         }
 
-        debounceTimeout = setTimeout(function() {
-            $.ajax({
-                url: 'https://suggestion.baidu.com/su?wd=' + keywords,
-                dataType: 'jsonp',
-                jsonp: 'cb',
-                beforeSend: function() {
-                    // 可以加入加载提示
-                },
-                success: function(res) {
-                    $word.empty().show();
-                    if (res.s && res.s.length > 0) {
-                        $word.css("display", "block");
-                        for (var i = 0; i < res.s.length; i++) {
-                            var li = $("<li><span>" + (i + 1) + "</span>" + res.s[i] + "</li>");
-
-                            // 添加不同的样式
-                            if (i === 0) {
-                                li.addClass("first-suggestion");
-                            } else if (i === 1) {
-                                li.addClass("second-suggestion");
-                            } else if (i === 2) {
-                                li.addClass("third-suggestion");
-                            }
-
-                            $word.append(li);
+        $.ajax({
+            url: 'https://suggestion.baidu.com/su?wd=' + keywords,
+            dataType: 'jsonp',
+            jsonp: 'cb',
+            beforeSend: function() {},
+            success: function(res) {
+                $word.empty().show();
+                var hotList = res.s.length;
+                if (hotList) {
+                    $word.css("display", "block");
+                    for (var i = 0; i < hotList-1; i++) {
+                        var li = $("<li><span>" + (i + 1) + "</span>" + res.s[i] + "</li>");
+                        if (i === 0) {
+                            li.css({ "border-top": "none" });
+                        } else if (i === 1) {
+                            li.find("span").css({ "color": "#fff", "background": "#ff8547" });
+                        } else if (i === 2) {
+                            li.find("span").css({ "color": "#fff", "background": "#ffac38" });
                         }
-                    } else {
-                        $word.hide();
+                        $word.append(li);
                     }
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    $word.empty().show();
+                } else {
                     $word.hide();
-                    alert("搜索建议加载失败，请稍后再试！");
                 }
-            });
-        }, 300); // 延迟 300ms
+            },
+            error: function() {
+                $word.empty().show();
+                $word.hide();
+            }
+        });
     });
 
     $word.on('click', 'li', function() {
@@ -61,34 +51,4 @@ $(function() {
     $(document).on('click', '.io-grey-mode', function() {
         $word.empty().hide();
     });
-
-    // 处理键盘事件
-    $searchText.keydown(function(e) {
-        var suggestions = $('#word li');
-        if (e.keyCode === 38) {  // 上箭头
-            if (currentIndex > 0) {
-                currentIndex--;
-                highlightSuggestion(currentIndex);
-            }
-        } else if (e.keyCode === 40) {  // 下箭头
-            if (currentIndex < suggestions.length - 1) {
-                currentIndex++;
-                highlightSuggestion(currentIndex);
-            }
-        } else if (e.keyCode === 13) {  // Enter键
-            selectSuggestion(currentIndex);
-        }
-    });
-
-    // 高亮当前选中的建议
-    function highlightSuggestion(index) {
-        $('#word li').removeClass('highlight').eq(index).addClass('highlight');
-    }
-
-    // 选择当前建议
-    function selectSuggestion(index) {
-        var selectedSuggestion = $('#word li').eq(index).text();
-        $searchText.val(selectedSuggestion);
-        $word.empty().hide();
-    }
 });
