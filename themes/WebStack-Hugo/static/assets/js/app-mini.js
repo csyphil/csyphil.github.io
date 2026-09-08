30cde6467d6ed089f909d9e4c964f63abaab3493
philsilver <philsilver31925@gmail.com>
2026-09-09 07:13:38 +0800

    update

--- themes/WebStack-Hugo/static/assets/js/app-mini.js
+++ themes/WebStack-Hugo/static/assets/js/app-mini.js
@@ -21,13 +21,6 @@
             trigger_resizable(false);
         //}
     }
-    $(document).on('click', "a[target!='_blank']", function() {
-        if( theme.loading=='1' && $(this).attr('href') && $(this).attr('href').indexOf("#") != 0 && $(this).attr('href').indexOf("java") != 0 && !$(this).data('fancybox')  && !$(this).data('commentid') && !$(this).hasClass('nofx') ){
-            var load = $('<div id="load-loading"></div>');
-            $("body").prepend(load);
-            load.animate({opacity:'1'},200,'swing').delay(2000).hide(300,function(){ load.remove() });
-        }
-    });
     //返回顶部
     $(window).scroll(function () {
         if ($(this).scrollTop() >= 50) {
